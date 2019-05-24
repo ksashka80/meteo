018c910f3c34af7594eeae762d52460a6edbf514
Александр Каргаполов <k.sashka80@yandex.ru>
2019-05-24 22:42:11 +0300

    Первичный commit проекта

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<HTML>
+
+    <HEAD>
+	<META charset="utf-8">
+	<LINK rel="stylesheet" type="text/css" href="styles/main.css">
+	<TITLE>Погода</TITLE>
+    </HEAD>
+
+    <BODY>
+	<HEADER>
+	</HEADER>
+
+	<NAV>
+	</NAV>
+
+	<MAIN>
+	</MAIN>
+
+	<FOOTER>
+	    &copy; Александр Каргаполов, 2019
+	</FOOTER>
+    </BODY>
+
+</HTML>
