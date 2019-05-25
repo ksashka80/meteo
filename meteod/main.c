79c0741b23b4562b297ad001c8a9c86d7941aa51
Александр Каргаполов <k.sashka80@yandex.ru>
2019-05-25 21:01:19 +0300

    Мелкие изменения

--- meteod/main.c
+++ meteod/main.c
@@ -1,5 +1,15 @@
-// FILE: 	
-// PROJECT:	
-// SUBPROJECT:	
+// FILE: 	main.c
+// PROJECT:	meteo
+// SUBPROJECT:	meteod
 // AUTHOR:	Alexandr Kargapolov
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "def.h"
+#include "main.h"
+
+int main(int argc,char* argv[])
+{
+    return RET_CODE_OK;
+}
