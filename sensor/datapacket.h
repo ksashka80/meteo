92780eca90d76c72003ccc2148d2228876b9334b
Александр Каргаполов <k.sashka80@yandex.ru>
2019-06-17 22:33:22 +0300

    Sensor

---
+++ sensor/datapacket.h
@@ -0,0 +1,26 @@
+// FILE: 	datapkt.h
+// PROJECT:	meteo
+// SUBPROJECT:	meteod
+// AUTHOR:	Alexandr Kargapolov
+
+#ifndef __DATAPKT_H__
+#define __DATAPKT_H__
+
+typedef struct
+{
+    int sensorID;
+    int sersorType;
+    float temperature;
+    float pressure;
+    float humidity;
+} DATA_BME280;
+
+typedef struct
+{
+    int pktVersion;
+    unsigned short crc;
+    DATA_BME280 data;
+} PKT_BME280;
+
+#endif // __DATAPKT_H__
+
